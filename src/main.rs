6e70bf791f93ad6cfd5cf1e3137f170d577fb8f3
Amir Ramezani <a.ramezani@kian.digital>
2025-08-12 11:31:56 +0330

    some limine stuff

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::arch::asm;
 use core::panic::PanicInfo;
+use limine::*;
 use limine::BaseRevision;
 use limine::request::*;
 
@@ -11,10 +12,97 @@ use limine::request::*;
 #[unsafe(link_section = ".requests")]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
 
+// boot loader information
+#[used]
+#[unsafe(link_section = ".requests")]
+static BOOTLOADER_INFO_REQUEST: BootloaderInfoRequest = BootloaderInfoRequest::new();
+
+// firmware type
+#[used]
+#[unsafe(link_section = ".requests")]
+static FIRMWARE_TYPE_REQUEST: FirmwareTypeRequest = FirmwareTypeRequest::new();
+
+// set stack size to 128 kb (will be back to it later)
+#[used]
+#[unsafe(link_section = ".requests")]
+static STACK_SIZE_REQUEST: StackSizeRequest = StackSizeRequest::new().with_size(128 * 1024);
+
+// hier half direct mapping
+#[used]
+#[unsafe(link_section = ".requests")]
+static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+
 // frame buffer
 #[used]
 #[unsafe(link_section = ".requests")]
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+// paging mode
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+#[used]
+#[unsafe(link_section = ".requests")]
+static PAGING_MODE_REQUEST: PagingModeRequest = PagingModeRequest::new().with_mode(paging::Mode::FOUR_LEVEL);
+
+#[cfg(target_arch = "riscv64")]
+#[used]
+#[unsafe(link_section = ".requests")]
+static PAGING_MODE_REQUEST: PagingModeRequest = PagingModeRequest::new().with_mode(paging::Mode::SV48);
+
+// bootstrap all cores on the system
+#[used]
+#[unsafe(link_section = ".requests")]
+static MP_REQUEST: MpRequest = MpRequest::new();
+
+// memory maps
+#[used]
+#[unsafe(link_section = ".requests")]
+static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
+
+// kernel information
+#[used]
+#[unsafe(link_section = ".requests")]
+static EXECUTABLE_FILE_REQUEST: ExecutableFileRequest = ExecutableFileRequest::new();
+
+// rsdp
+#[used]
+#[unsafe(link_section = ".requests")]
+static RSDP_REQUEST: RsdpRequest = RsdpRequest::new();
+
+// smbios information
+#[used]
+#[unsafe(link_section = ".requests")]
+static SMBIOS_REQUEST: SmbiosRequest = SmbiosRequest::new();
+
+// uefi table
+#[used]
+#[unsafe(link_section = ".requests")]
+static EFI_SYSTEM_TABLE_REQUEST: EfiSystemTableRequest = EfiSystemTableRequest::new();
+
+// uefi memory map
+#[used]
+#[unsafe(link_section = ".requests")]
+static EFI_MEMORY_MAP_REQUEST: EfiMemoryMapRequest = EfiMemoryMapRequest::new();
+
+// boot time
+#[used]
+#[unsafe(link_section = ".requests")]
+static DATE_AT_BOOT_REQUEST: DateAtBootRequest = DateAtBootRequest::new();
+
+// kernel address
+#[used]
+#[unsafe(link_section = ".requests")]
+static EXECUTABLE_ADDRESS_REQUEST: ExecutableAddressRequest = ExecutableAddressRequest::new();
+
+// device tree blob
+#[used]
+#[unsafe(link_section = ".requests")]
+static DEVICE_TREE_BLOB_REQUEST: DeviceTreeBlobRequest = DeviceTreeBlobRequest::new();
+
+// bsp Hart ID for riscv64
+#[cfg(target_arch = "riscv64")]
+#[used]
+#[unsafe(link_section = ".requests")]
+static BSP_HARTID_REQUEST: BspHartidRequest = BspHartidRequest::new();
 
 // start and end markers for limine
 #[used]
